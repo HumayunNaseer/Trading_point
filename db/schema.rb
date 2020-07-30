288c7cdff8b72c8f389ad7de016bbb7752ed7428
Humayun Naseer <humayunnaseer5@gmail.com>
2020-07-30 14:55:42 +0500

    product table complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_29_174219) do
+ActiveRecord::Schema.define(version: 2020_07_30_065148) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "productname"
+    t.integer "productprice"
+    t.boolean "productstatus"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
